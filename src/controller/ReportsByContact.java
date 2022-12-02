e95a1078a54b0504a7d4bdcbe8cac6e6ea254441
Osvaldo Ibarra <oibarr@outlook.com>
2022-12-01 22:55:11 -0600

    "Refactored reports controllers"

--- src/controller/ReportsByContact.java
+++ src/controller/ReportsByContact.java
@@ -1,48 +1,34 @@
 package controller;
 
 import dao.AppointmentDAO;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import model.Appointment;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class ReportsByContact extends ReportsDashboard implements Initializable {
-
-    ObservableList<Appointment> appointmentContacts = FXCollections.observableArrayList();
-    ObservableList<Appointment> appointmentsByContact = FXCollections.observableArrayList();
-
-    private void filterByContact(int contID){
-        for(Appointment a : allAppointments){
-            if(a.getAppContId() == contID){
-                appointmentsByContact.add(a);
+public class ReportsByContact extends ReportsByMonth implements Initializable {
+    @Override
+    public void filterSelection(Appointment appointment) {
+        int totalCount = 0;
+        for (Appointment a : allAppointments) {
+            if (a.getAppContId() == appointment.getAppContId()){
+                appointmentsByFilter.add(a);
+                totalCount++;
             }
         }
+        countLabel.setText("Total Count: " + totalCount);
     }
 
     @Override
-    public void onActionCombo(ActionEvent actionEvent) {
-        appointmentsByContact.clear();
-        filterByContact(appCombo.getSelectionModel().getSelectedItem().getAppContId());
-        appTable.setItems(appointmentsByContact);
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        super.initialize(url, resourceBundle);
+    public void initializeReportScreen() {
         appLabel.setText(appLabel.getText() + " by Contact");
         appCombo.setPromptText("Contact");
 
         try {
-            appointmentContacts.addAll(AppointmentDAO.getAppointmentContacts());
+            appointmentFilter.addAll(AppointmentDAO.getAppointmentContacts());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        appCombo.setItems(appointmentContacts);
-        System.out.println("Appointment Contacts: " + appointmentContacts);
-
+        appCombo.setItems(appointmentFilter);
+        System.out.println("Appointment Contacts: " + appointmentFilter);
     }
+
 }
--- src/controller/ReportsByMonth.java
+++ src/controller/ReportsByMonth.java
@@ -1,46 +1,35 @@
 package controller;
 
 import dao.AppointmentDAO;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import model.Appointment;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class ReportsByMonth extends ReportsDashboard implements Initializable {
-    ObservableList<Appointment> appointmentMonths = FXCollections.observableArrayList();
-    ObservableList<Appointment> appointmentsByMonth = FXCollections.observableArrayList();
-    private void filterByMonth(Appointment appointment){
-        for(Appointment a : allAppointments){
-            if(a.getAppStart().getMonth().equals(appointment.getAppStart().getMonth())){
-                appointmentsByMonth.add(a);
+public class ReportsByMonth extends ReportsByType implements Initializable {
+    @Override
+    public void filterSelection(Appointment appointment) {
+        int totalCount = 0;
+        for (Appointment a : allAppointments) {
+            if (a.getAppStart().getMonth().equals(appointment.getAppStart().getMonth())){
+                appointmentsByFilter.add(a);
+                totalCount++;
             }
         }
+        countLabel.setText("Total Count: " + totalCount);
     }
 
-    @Override
-    public void onActionCombo(ActionEvent actionEvent){
-        appointmentsByMonth.clear();
-        filterByMonth(appCombo.getSelectionModel().getSelectedItem());
-        appTable.setItems(appointmentsByMonth);
-    }
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        super.initialize(url, resourceBundle);
+    public void initializeReportScreen() {
         appLabel.setText(appLabel.getText() + " by Month");
         appCombo.setPromptText("Month");
 
         try {
-            appointmentMonths.addAll(AppointmentDAO.getMonthsFromAppointments());
+            appointmentFilter.addAll(AppointmentDAO.getMonthsFromAppointments());
         } catch (Exception e) {
             e.printStackTrace();
         }
-        appCombo.setItems(appointmentMonths);
-        System.out.println("Appointment Months: " + appointmentMonths);
-
+        appCombo.setItems(appointmentFilter);
+        System.out.println("Appointment Months: " + appointmentFilter);
     }
+
 }
